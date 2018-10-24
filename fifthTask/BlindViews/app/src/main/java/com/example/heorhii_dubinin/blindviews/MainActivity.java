d7a02d808220f899009d76834e864c5370d04d1e
Heorhii Dubinin <heorhii_dubinin@epam.com>
2018-10-24 15:18:43 +0300

    progrmatical view

--- fifthTask/BlindViews/app/src/main/java/com/example/heorhii_dubinin/blindviews/MainActivity.java
+++ fifthTask/BlindViews/app/src/main/java/com/example/heorhii_dubinin/blindviews/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.heorhii_dubinin.blindviews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        drawingUI();
+    }
+
+    private void drawingUI() {
+
+        LinearLayout linLayout = new LinearLayout(this);
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams linLayoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        setContentView(linLayout, linLayoutParam);
+
+        LinearLayout.LayoutParams lpView = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView tv = new TextView(this);
+        tv.setText(R.string.hello_moon);
+        tv.setLayoutParams(lpView);
+        linLayout.addView(tv);
+
+        Button btn1 = new Button(this);
+        btn1.setText(R.string.calculators);
+        linLayout.addView(btn1, lpView);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button btn2 = new Button(this);
+        btn2.setText(R.string.add_elements);
+        linLayout.addView(btn2, lpView);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
