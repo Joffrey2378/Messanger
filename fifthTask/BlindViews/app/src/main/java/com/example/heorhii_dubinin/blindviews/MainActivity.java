37760f6901aa0940dfe7c9390af7a2e4b20a2191
Heorhii Dubinin <heorhii_dubinin@epam.com>
2018-10-24 18:54:14 +0300

    second activity and asinchLayoutInfalaer implemented

--- fifthTask/BlindViews/app/src/main/java/com/example/heorhii_dubinin/blindviews/MainActivity.java
+++ fifthTask/BlindViews/app/src/main/java/com/example/heorhii_dubinin/blindviews/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.heorhii_dubinin.blindviews;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.AsyncLayoutInflater;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,16 +16,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         drawingUI();
     }
 
     private void drawingUI() {
 
-        LinearLayout linLayout = new LinearLayout(this);
+        final ScrollView scrollView = new ScrollView(this);
+
+        final LinearLayout linLayout = new LinearLayout(this);
         linLayout.setOrientation(LinearLayout.VERTICAL);
+
         LinearLayout.LayoutParams linLayoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        setContentView(linLayout, linLayoutParam);
+        scrollView.addView(linLayout, linLayoutParam);
+        setContentView(scrollView, linLayoutParam);
 
         LinearLayout.LayoutParams lpView = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
@@ -30,14 +37,14 @@ public class MainActivity extends AppCompatActivity {
         tv.setLayoutParams(lpView);
         linLayout.addView(tv);
 
-        Button btn1 = new Button(this);
+        final Button btn1 = new Button(this);
         btn1.setText(R.string.calculators);
         linLayout.addView(btn1, lpView);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(getApplicationContext(), SecondActivity.class));
             }
         });
 
@@ -48,8 +55,24 @@ public class MainActivity extends AppCompatActivity {
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final AsyncLayoutInflater asyncLayoutInflater = new AsyncLayoutInflater(getApplicationContext());
+                asyncLayoutInflater.inflate(R.layout.custom_activity, linLayout, new AsyncLayoutInflater.OnInflateFinishedListener() {
+                    @Override
+                    public void onInflateFinished(View view, int resid, ViewGroup parent) {
+                        parent.addView(view);
+                    }
+                });
 
             }
         });
     }
+//    linearLayout.setOnTouchListener(new View.OnTouchListener() {
+//        @Override
+//        public boolean onTouch(View v, MotionEvent event) {
+//            Intent intent = new Intent(SecondActivity.this,SevenColorRainbowActivity.class);
+//            startActivity(intent);
+//            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+//            return true;
+//        }
+//    }
 }
