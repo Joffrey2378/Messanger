ff41df8ebaa02cc26e5035d7e79cb1323d1e5db6
Heorhii Dubinin <heorhii_dubinin@epam.com>
2018-10-29 12:59:02 +0200

    implemented: attaching different layouts to one activity, time measuring, some view fixes

--- fifthTask/BlindViews/app/src/main/java/com/example/heorhii_dubinin/blindviews/CalculatorLayoutActivity.java
+++ fifthTask/BlindViews/app/src/main/java/com/example/heorhii_dubinin/blindviews/CalculatorLayoutActivity.java
@@ -1,14 +1,42 @@
 package com.example.heorhii_dubinin.blindviews;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class CalculatorLayoutActivity extends AppCompatActivity {
 
+    public static final String KEY_ROOT_LAYOUT = "KEY_ROOT_LAYOUT";
+
+    public static Intent newInstance(Context context, int activityResource) {
+        Intent intent = new Intent(context, CalculatorLayoutActivity.class);
+        intent.putExtra(KEY_ROOT_LAYOUT, activityResource);
+        return intent;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        long startTime = System.currentTimeMillis();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frame_layout);
+        setContentView(getLayoutIdFromIntent());
+        long endTime = System.currentTimeMillis();
+        EditText editText = findViewById(R.id.editText3);
+        editText.setText("t= " + (endTime - startTime));
+    }
+
+    private int getLayoutIdFromIntent() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            int layoutId = intent.getIntExtra(KEY_ROOT_LAYOUT, -1);
+            if (layoutId == -1) {
+                throw new RuntimeException("No layout id was provided.");
+            } else {
+                return layoutId;
+            }
+        }
+        throw new RuntimeException("No extras were provided.");
     }
 
 //    public static final String KEY_TEXT = "KEY_TEXT";
--- fifthTask/BlindViews/app/src/main/java/com/example/heorhii_dubinin/blindviews/SecondActivity.java
+++ fifthTask/BlindViews/app/src/main/java/com/example/heorhii_dubinin/blindviews/SecondActivity.java
@@ -40,10 +40,12 @@ public class SecondActivity extends AppCompatActivity {
         rainbowTextView = new TextView(this);
         rainbowTextView.setLayoutParams(lpView);
         linLayout.addView(rainbowTextView);
+        rainbowTextView.setPadding(20, 20, 20, 20);
 
         splashTextView = new TextView(this);
         splashTextView.setLayoutParams(lpView);
         linLayout.addView(splashTextView);
+        splashTextView.setPadding(20, 50, 0, 0);
 
         magicString();
     }
@@ -63,10 +65,10 @@ public class SecondActivity extends AppCompatActivity {
         rainbowTextView.setText(spannableColor);
 
         SpannableString spannableString = new SpannableString(rainbowTextView.getText());
-        ClickableSpan clickableSpan = new ClickableSpan() {
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
             @Override
             public void onClick(View textView) {
-                startActivity(new Intent(SecondActivity.this, CalculatorLayoutActivity.class));
+                startActivity(CalculatorLayoutActivity.newInstance(SecondActivity.this, R.layout.activity_frame_layout));
             }
 
             @Override
@@ -75,13 +77,55 @@ public class SecondActivity extends AppCompatActivity {
                 ds.setUnderlineText(true);
             }
         };
-        spannableString.setSpan(clickableSpan, 105, 117, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(clickableSpan1, 105, 117, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        ClickableSpan clickableSpan2 = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                startActivity(CalculatorLayoutActivity.newInstance(SecondActivity.this, R.layout.activity_linear_layout));
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(true);
+            }
+        };
+        spannableString.setSpan(clickableSpan2, 119, 131, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        ClickableSpan clickableSpan3 = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                startActivity(CalculatorLayoutActivity.newInstance(SecondActivity.this, R.layout.activity_relative_layout));
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(true);
+            }
+        };
+        spannableString.setSpan(clickableSpan3, 133, 147, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                startActivity(CalculatorLayoutActivity.newInstance(SecondActivity.this, R.layout.activity_constraint_layout));
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(true);
+            }
+        };
+        spannableString.setSpan(clickableSpan, 149, 165, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         rainbowTextView.setText(spannableString);
         rainbowTextView.setMovementMethod(LinkMovementMethod.getInstance());
 
         SpannableString ss = new SpannableString("Show me Splash please.");
-        ClickableSpan clickableSpan2 = new ClickableSpan() {
+        ClickableSpan clickableSpan2Test = new ClickableSpan() {
             @Override
             public void onClick(View splashTextView) {
                 startActivity(new Intent(SecondActivity.this, SplashActivity.class));
@@ -93,7 +137,7 @@ public class SecondActivity extends AppCompatActivity {
                 ds.setUnderlineText(true);
             }
         };
-        ss.setSpan(clickableSpan2, 8, 14, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(clickableSpan2Test, 8, 14, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         splashTextView.setText(ss);
         splashTextView.setMovementMethod(LinkMovementMethod.getInstance());
