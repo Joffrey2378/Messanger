1cc4fed2a8b07225437591d40ea324c8aa01fb44
Joffrey2378 <georged.2378@gmail.com>
2019-01-18 07:29:52 +0200

    sending messages to firebase via text editor in main activity was performed

--- xvTask/MessAges/app/src/main/java/com/example/messages/ui/MainActivity.java
+++ xvTask/MessAges/app/src/main/java/com/example/messages/ui/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.messages.MessageModel;
 import com.example.messages.R;
@@ -13,17 +18,66 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String USER_NAME = "admin";
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference messageReference = db.collection("chat");
 
     private MessageAdapter adapter;
+
+    private EditText textMessage;
+    private Button btnSend;
+
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         setupRecyclerView();
+
+        initializeViews();
+
+        editTextSetups();
+
+        btnSend.setOnClickListener(this::pushMessage);
+    }
+
+    private void initializeViews() {
+        textMessage = findViewById(R.id.messageEditText);
+        btnSend = findViewById(R.id.sendButton);
+        userName = USER_NAME;
+    }
+
+    private void pushMessage(View view) {
+        MessageModel messageModel = new MessageModel(null, textMessage.getText().toString(), userName, null);
+        messageReference.add(messageModel);
+
+        textMessage.setText("");
+    }
+
+    private void editTextSetups() {
+        textMessage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    btnSend.setEnabled(true);
+                } else {
+                    btnSend.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
     private void setupRecyclerView() {
