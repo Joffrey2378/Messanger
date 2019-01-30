e216b1192c103acf6f1f1952cf5fd0bbdbce406b
Heorhii Dubinin <heorhii_dubinin@epam.com>
2019-01-30 16:21:04 +0200

    firebase service was created

---
+++ xvTask/MessAges/app/src/main/java/com/example/messages/MyFirebaseService.java
@@ -0,0 +1,15 @@
+package com.example.messages;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
